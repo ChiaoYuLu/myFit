27009b30a1ba9071efba599298a8dcf8cda28aca
Chiao Yu <chyu.inbox@gmial.com>
2025-01-16 11:21:18 +0100

    bmiCaculater() aanMaken

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'myFit';
+  age: any;
+  weight: any;
+  height: any;
+  gender: any;
+  bmi: any;
+
+  constructor(){
+    this.bmi = 0;
+  }
+
+  bmiCalculate(){
+    if (this.weight && this.height){
+      this.bmi = (this.weight / ((this.height * this.height)/10000)).toFixed(1);
+    }
+  }
 }
